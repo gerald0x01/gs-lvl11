2ac65ac5e2699baf531953e37ea3535b6d666ddd
gerald0x01 <geraldoahnert@gmail.com>
2020-09-28 18:51:26 -0300

    Add: Components.

---
+++ front-end/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Header from "./components/Header";
+
+function App() {
+  const [projects, setProjects] = useState([
+    "App Development",
+    "Web Front-End",
+  ]);
+  // UseState retorna um array com 2 posições.
+  // 1. Variável com o seu valor inicial.
+  // 2. Função para atualizarmos esse valor.
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
+    </>
+  );
+}
+
+export default App;
